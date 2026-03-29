eef76b204f8b28c205f42a2011640b14c1156725
Noodl3r <Echelon.r6s@gmail.com>
2026-03-29 15:08:49 -0400

    initialization

---
+++ sim_1/new/gibbaGoob_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2026 03:07:22 PM
+// Design Name: 
+// Module Name: gibbaGoob_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gibbaGoob_tb(
+
+    );
+endmodule
