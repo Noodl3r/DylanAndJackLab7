60248b80f5b7d0acd4cc3e0990ed47c34d862983
Noodl3r <Echelon.r6s@gmail.com>
2026-03-29 15:19:10 -0400

    First stuff

---
+++ sim_1/new/Schwunk.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2026 03:14:26 PM
+// Design Name: 
+// Module Name: Schwunk
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Schwunk(
+
+    );
+endmodule
